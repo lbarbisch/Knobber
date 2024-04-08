88281c89fd8c22cb9f671e6d07049caf5bc19d4f
Florin Tobler <florin.tobler@hotmail.com>
2024-04-08 22:34:42 +0200

    rtos task change interrupt

--- Software/Knobber_STM32/Core/Startup/startup_stm32g070rbtx.s
+++ Software/Knobber_STM32/Core/Startup/startup_stm32g070rbtx.s
@@ -143,7 +143,8 @@ g_pfnVectors:
   .word SVC_Handler
   .word 0
   .word 0
-  .word PendSV_Handler
+  /*.word PendSV_Handler*/
+  .word scheduler_pendSV_handler /* flortos */
   .word SysTick_Handler
   .word WWDG_IRQHandler                   /* Window WatchDog              */
   .word 0                                /* reserved                     */
